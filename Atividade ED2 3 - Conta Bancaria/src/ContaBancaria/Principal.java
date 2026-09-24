997bfe5220a924cef2a0a908484f38d1600d3432
caiomcancela <caiomcancela.18@gmail.com>
2026-09-24 08:12:15 -0300

    02 | 03 Atividade Prática

---
+++ Atividade ED2 3 - Conta Bancaria/src/ContaBancaria/Principal.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ContaBancaria;
+
+
+public class Principal {
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> new Janela().setVisible(true));
+    }
+}
